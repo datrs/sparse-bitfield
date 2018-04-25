112a57a34e369849e0b29a61e09549561dea54c8
Tyler Neely <t@jujit.su>
2018-04-25 14:02:37 +0200

    Add simple proptest model, fix a couple bugs in expected page size.

---
+++ tests/model.rs
@@ -0,0 +1,18 @@
+extern crate sparse_bitfield;
+#[macro_use]
+extern crate proptest;
+
+use sparse_bitfield::{Bitfield, Change};
+
+fn model(bit: usize, page_sz: usize) {
+  let mut bits = Bitfield::new(page_sz);
+  assert_eq!(bits.set(bit, true), Change::Changed);
+  assert_eq!(bits.get(bit), true);
+}
+
+proptest! {
+  #[test]
+  fn doesnt_crash(bit in 0usize..1_000_000, page_sz_exponent in 0usize..30) {
+    model(bit, 1 << page_sz_exponent);
+  }
+}
